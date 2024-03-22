3afdab160dde50b43f68a64df48c1fd1419223a6
Dmytro Herashchenko <dmytro.duck@gmail.com>
2024-03-22 19:30:35 +0200

    feat: display all available coupons with filtering options

--- src/routes/fora/coupons/+page.server.js
+++ src/routes/fora/coupons/+page.server.js
@@ -2,115 +2,35 @@ import { coupons } from "$lib/schema/fora";
 import { users } from "$lib/schema/public";
 import { db } from "$lib/server/db";
 import { REFERRAL_MINIMUM_SPEND } from "$lib/server/fora";
-import { error, redirect } from "@sveltejs/kit";
-import { and, count, eq, gt, inArray, isNull, or, sql } from "drizzle-orm";
+import { redirect } from "@sveltejs/kit";
+import { and, eq, gt, isNull, or, sql } from "drizzle-orm";
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ locals }) {
 	if (!locals.userId) {
-		error(401, "Unauthorized");
+		redirect(302, "/auth");
 	}
-	return {
-		minimumSpend: REFERRAL_MINIMUM_SPEND,
-		coupons: await fetchAvailableCoupons(locals.userId),
-		streamed: {
-			coupons: fetchViewableCoupons(locals.userId),
-		},
-	};
-}
 
-/** @type {import('./$types').Actions} */
-export const actions = {
-	default: async ({ locals, url }) => {
-		if (!locals.userId) {
-			error(401, "Unauthorized");
-		}
-
-		const isReferral = url.searchParams.get("is_referral") === "true";
-		if (isReferral === null) {
-			error(400, "Missing Is Referral");
-		}
-
-		const couponId = await assignCouponByIsReferral(locals.userId, isReferral);
-		if (couponId) {
-			redirect(303, `fora/coupon/${couponId}`);
-		}
-		error(409, "Out Of Coupons");
-	},
-};
-
-/**
- * Counts all available coupons, that can be assigned to the user
- * @param {string} userId
- * @returns
- */
-async function fetchAvailableCoupons(userId) {
-	const is_same_family = or(eq(coupons.familyId, users.familyId), isNull(coupons.familyId));
-	const data = await db
-		.select({ isReferral: coupons.isReferral, count: count() })
-		.from(coupons)
-		.innerJoin(users, and(eq(users.id, userId), is_same_family))
-		.where(and(eq(coupons.status, "available"), gt(coupons.expiredAt, sql`now()`)))
-		.groupBy(coupons.isReferral);
-
-	return [
-		{
-			discount: "50+",
-			isReferral: false,
-			count: data.find((e) => !e.isReferral)?.count ?? 0,
-		},
-		{
-			discount: "0 або 100",
-			isReferral: true,
-			count: data.find((e) => e.isReferral)?.count ?? 0,
-		},
-	];
-}
-
-/**
- * Fetches all coupons that the user can view
- * @param {string} userId
- * @returns
- */
-async function fetchViewableCoupons(userId) {
-	return await db
+	const available = await db
 		.select({
 			id: coupons.id,
-			status: coupons.status,
 			discount: coupons.discount,
-			isReferral: coupons.isReferral,
 			expiredAt: coupons.expiredAt,
+			isReferral: coupons.isReferral,
 		})
 		.from(coupons)
-		.where(
+		.innerJoin(
+			users,
 			and(
-				eq(coupons.userId, userId),
-				inArray(coupons.status, ["assigned", "hidden"]),
-				gt(coupons.expiredAt, sql`now()`),
+				eq(users.id, locals.userId),
+				or(eq(coupons.familyId, users.familyId), isNull(coupons.familyId)),
 			),
-		);
-}
+		)
+		.where(and(eq(coupons.status, "available"), gt(coupons.expiredAt, sql`now()`)))
+		.orderBy(coupons.expiredAt);
 
-/**
- * Assigns a new coupon to the user with the specified `isReferral`
- * @param {string} userId
- * @param {boolean} isReferral
- * @returns {Promise<string | undefined>}
- */
-async function assignCouponByIsReferral(userId, isReferral) {
-	const is_available = and(eq(coupons.status, "available"), gt(coupons.expiredAt, sql`now()`));
-	const is_same_family = or(eq(coupons.familyId, users.familyId), isNull(coupons.familyId));
-	const select_coupon = db
-		.select({ id: coupons.id })
-		.from(coupons)
-		.innerJoin(users, eq(users.id, userId))
-		.where(and(is_available, is_same_family, eq(coupons.isReferral, isReferral)))
-		.limit(1)
-		.for("update", { skipLocked: true });
-	const data = await db
-		.update(coupons)
-		.set({ status: "assigned", userId: userId })
-		.where(eq(coupons.id, select_coupon))
-		.returning({ id: coupons.id });
-	return data.length ? data[0].id : undefined;
+	return {
+		REFERRAL_MINIMUM_SPEND,
+		coupons: available,
+	};
 }
--- src/routes/fora/coupons/+page.svelte
+++ src/routes/fora/coupons/+page.svelte
@@ -5,99 +5,96 @@
 	/** @type {import('./$types').PageData} */
 	export let data;
 
+	let filter = -1;
+
+	$: coupons = filterCoupons(filter, data.coupons);
+
+	/**
+	 * @param {number} value
+	 * @param {typeof data.coupons} coupons
+	 */
+	function filterCoupons(value, coupons) {
+		if (value === -1) {
+			return coupons;
+		}
+		return coupons.filter(
+			(c) => parseFloat(c.discount) >= filter && parseFloat(c.discount) < filter + 50,
+		);
+	}
+
 	const formatter = new Intl.DateTimeFormat("uk-UA", {
 		day: "numeric",
 		month: "long",
 	});
 </script>
 
-<form action="" method="post" use:enhance>
-	{#each data.coupons as coupon}
-		<Coupon>
-			<svelte:fragment slot="left">
-				{coupon.discount} грн.
-				<br />
-				{#if coupon.isReferral}
-					Покупка понад {data.minimumSpend} грн.
-				{:else}
-					Немає мінімальної суми
-				{/if}
-				<br />
-				В наявності {coupon.count} шт.
-			</svelte:fragment>
-			<button
-				slot="right"
-				type="submit"
-				class="action"
-				formaction="?is_referral={coupon.isReferral}"
-				disabled={!coupon.count}>Отримати</button
-			>
-		</Coupon>
+<header>
+	<input type="radio" id="all" name="price" value={-1} bind:group={filter} />
+	<label for="all">Всі</label>
+	{#each [0, 50, 100] as p}
+		<input type="radio" id="price{p}" name="price" value={p} bind:group={filter} />
+		<label for="price{p}">{p}-{p + 49}</label>
 	{/each}
-</form>
-{#await data.streamed.coupons}
-	<h3 class="skeleton">Ваші купони</h3>
-	<Coupon>
-		<svelte:fragment slot="left">
-			<span class="skeleton">Будь ласка</span><br />
-			<span class="skeleton">Зачекайте поки Ваші</span><br />
-			<span class="skeleton">Купони завантажуються</span>
-		</svelte:fragment>
-		<span slot="right" class="action skeleton">Перейти</span>
-	</Coupon>
-{:then coupons}
-	<h3>Ваші купони</h3>
+</header>
+
+<form method="post" use:enhance>
+	<input type="hidden" name="status" value="assigned" />
 	{#each coupons as coupon (coupon.id)}
 		<Coupon>
 			<svelte:fragment slot="left">
 				{coupon.discount} грн.
 				<br />
 				{#if coupon.isReferral}
-					Покупка понад {data.minimumSpend} грн.
+					Покупка понад {data.REFERRAL_MINIMUM_SPEND} грн.
 				{:else}
 					Немає мінімальної суми
 				{/if}
 				<br />
 				Дійсний до {formatter.format(coupon.expiredAt)}
 			</svelte:fragment>
-			<a
-				slot="right"
-				class="action"
-				class:marked={coupon.status === "hidden"}
-				data-sveltekit-preload-data="false"
-				href="fora/coupon/{coupon.id}">Перейти</a
-			>
+			<button slot="right" formaction="coupons/{coupon.id}">Перейти</button>
 		</Coupon>
 	{:else}
-		<h3>Отримайте нові купони ☝️</h3>
+		<h3>Обраних купонів не знайдено</h3>
 	{/each}
-{/await}
+</form>
 
 <style>
-	.action {
-		background-color: var(--color-lime-500);
-		color: var(--color-text-accent);
-		padding: 0.5rem;
+	header {
+		display: flex;
+		flex-wrap: nowrap;
+		gap: 0.5rem;
+		padding: 1rem 1rem 0.5rem;
+	}
+	form {
+		padding-inline: 1rem;
+	}
+	input[type="radio"] {
+		display: none;
+	}
+	label {
+		background-color: var(--color-surface-section);
+		min-width: 2rem;
+	}
+	button,
+	label {
+		padding: 0.5rem 1rem;
 		border-radius: 1rem;
 		font-size: 1rem;
 		line-height: 1rem;
-		font-weight: var(--fw-bold);
-		box-shadow: 0 0.25rem 0 var(--color-surface-ground);
-	}
-	a {
-		text-decoration: none;
+		cursor: pointer;
+		user-select: none;
+		-webkit-tap-highlight-color: transparent;
+		box-shadow: 0 0 1rem rgb(0 0 0 / 0.122);
 	}
 	button {
-		cursor: pointer;
 		outline: none;
 		border: none;
 	}
-	.marked,
-	button:disabled {
-		background-color: var(--color-hint);
-		pointer-events: none;
-		cursor: not-allowed;
-		opacity: 0.7;
-		box-shadow: none;
+	input[type="radio"]:checked + label,
+	button {
+		font-weight: 600;
+		background-color: var(--color-accent);
+		color: var(--color-text-accent);
 	}
 </style>
