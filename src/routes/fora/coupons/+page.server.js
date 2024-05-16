6e265795c8c813d2af8e020c24ee482bffbeb5b6
Dmytro Herashchenko <dmytro.anyduck@gmail.com>
2024-05-16 20:13:17 +0300

    Revert "fix: coupons are valid through expire date"
    Turns out they're not 😟
    
    This reverts commit ab6f2358652dd458b360089239babb39011a4379.

--- src/routes/fora/coupons/+page.server.js
+++ src/routes/fora/coupons/+page.server.js
@@ -3,7 +3,7 @@ import { users } from "$lib/schema/public";
 import { db } from "$lib/server/db";
 import { REFERRAL_MINIMUM_SPEND } from "$lib/server/fora";
 import { error } from "@sveltejs/kit";
-import { and, eq, gte, isNull, or, sql } from "drizzle-orm";
+import { and, eq, gt, isNull, or, sql } from "drizzle-orm";
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ locals }) {
@@ -26,7 +26,7 @@ export async function load({ locals }) {
 				or(eq(coupons.familyId, users.familyId), isNull(coupons.familyId)),
 			),
 		)
-		.where(and(eq(coupons.status, "available"), gte(coupons.expiredAt, sql`CURRENT_DATE`)))
+		.where(and(eq(coupons.status, "available"), gt(coupons.expiredAt, sql`now()`)))
 		.orderBy(coupons.expiredAt);
 
 	return {
